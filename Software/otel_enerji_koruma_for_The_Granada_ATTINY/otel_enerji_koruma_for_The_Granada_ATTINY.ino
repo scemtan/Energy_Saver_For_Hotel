442167523894a9b27866803e11925b520dd984c9
SÜHEYL CEM TAN <39568762+scemtan@users.noreply.github.com>
2021-02-12 07:37:53 +0300

    3e-turkey Project
    * Revision

--- Software/otel_enerji_koruma_for_The_Granada_ATTINY/otel_enerji_koruma_for_The_Granada_ATTINY.ino
+++ Software/otel_enerji_koruma_for_The_Granada_ATTINY/otel_enerji_koruma_for_The_Granada_ATTINY.ino
@@ -1,38 +1,36 @@
-#define relay  2 
+#define relay  4 
 #define DOOR   0 
-#define BEING 1
+#define BEING 2
 int x = 0;
 unsigned long old_time_1 = 0, old_time_2 = 0;
 unsigned long now_time_1 = 0, now_time_2 = 0;
 unsigned long day_time  = 60000; // 86400000 == 24H // 43200000 == 12H // 21600000 == 6H // 3600000 == 1H // 1800000 == 1/2H
-unsigned long room_time = 15000;
-void setup() {pinMode(BEING, INPUT);pinMode(DOOR, INPUT);pinMode(relay, OUTPUT);digitalWrite(DOOR,HIGH);digitalWrite(BEING,HIGH);delay(2500);}
+unsigned long room_time = 30000;
+void setup() {pinMode(BEING, INPUT);pinMode(DOOR, INPUT);pinMode(relay, OUTPUT);digitalWrite(DOOR,LOW);digitalWrite(BEING,LOW);delay(2500);}
 void loop() {  
 // ***************************************** //
-if (digitalRead(DOOR) == LOW)
+if (digitalRead(DOOR) == HIGH)
 { 
-  if (digitalRead(BEING) == LOW){x = 1;old_time_1 = millis();}
-  else if (digitalRead(BEING) == HIGH){now_time_1 = millis();if ((now_time_1 - old_time_1 == day_time) && (digitalRead(BEING) == HIGH) ){x = 0;old_time_1 = millis();}}
+  if (digitalRead(BEING) == HIGH){x = 1;old_time_1 = millis();}
+  else if (digitalRead(BEING) == LOW){now_time_1 = millis();if ((now_time_1 - old_time_1 == day_time) && (digitalRead(BEING) == LOW) ){x = 0;old_time_1 = millis();}}
   else{ }
 }
 // ***************************************** //
-if (digitalRead(DOOR) == HIGH)
+if (digitalRead(DOOR) == LOW)
 {
   x = 2;
 }
 // ***************************************** //
 if (x == 2 )
 {
-  if ((digitalRead(DOOR) == LOW) && (digitalRead(BEING) == HIGH)){x = 3;}
+  if ((digitalRead(DOOR) == HIGH) && (digitalRead(BEING) == LOW)){x = 3;}
 }
 // ***************************************** //
-if (x == 0) {digitalWrite(relay, HIGH);}
-if (x == 1) {digitalWrite(relay, LOW);}
+if (x == 0) {digitalWrite(relay, LOW);}
+if (x == 1) {digitalWrite(relay, HIGH);}
 //if (x == 2) {}
 if (x == 3) {now_time_1 = millis();if (now_time_1 - old_time_1 == (room_time)){
-             if ((digitalRead(BEING) == LOW)){old_time_1 = millis();x = 1;}
+             if ((digitalRead(BEING) == HIGH)){old_time_1 = millis();x = 1;}
              if (x !=1){x = 0;}old_time_1 = millis();}}
-//if (x == 3) {for (int sure = 0; sure < 10000; sure++){delay(1);if ((digitalRead(KAPI) == HIGH) && (digitalRead(VARLIK) == HIGH)){ x = 4;Serial.println("İçeri Girildi");}}if (x != 4) {x = 0;Serial.println("Röle Kapandı");}}
-//if (x == 4) {x = 1;}
 // ***************************************** //
 }
