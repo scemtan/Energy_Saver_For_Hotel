aedeef53e9adfe83046b7c94e3fe19c65094401c
SÜHEYL CEM TAN <39568762+scemtan@users.noreply.github.com>
2021-02-16 01:22:41 +0300

    3e-turkey Project
    * Revision

--- Software/otel_enerji_koruma_for_The_Granada_ATMEL/otel_enerji_koruma_for_The_Granada_ATMEL.ino
+++ Software/otel_enerji_koruma_for_The_Granada_ATMEL/otel_enerji_koruma_for_The_Granada_ATMEL.ino
@@ -5,7 +5,7 @@ int x = 0, y = 0;
 unsigned long old_time_1 = 0, old_time_2 = 0;
 unsigned long now_time_1 = 0, now_time_2 = 0;
 unsigned long day_time  = 86400000; // 86400000 == 24H // 43200000 == 12H // 21600000 == 6H // 3600000 == 1H // 1800000 == 1/2H
-unsigned long room_time = 45000;
+unsigned long room_time = 15000;
 void setup() {Serial.begin(115200);pinMode(BEING, INPUT);pinMode(DOOR, INPUT);pinMode(relay, OUTPUT);digitalWrite(DOOR,LOW);digitalWrite(BEING,LOW);delay(5000);Serial.println("Sistem Başlatıldı");}
 void loop() {  
 // ***************************************** //
@@ -26,9 +26,9 @@ x = 3;
 if (x == 3 && digitalRead(BEING) == HIGH){old_time_1 = millis();Serial.println("Varlık Algılandı");x = 1;}
 if (x == 3 && digitalRead(BEING) != HIGH){now_time_1 = millis();if ((now_time_1 - old_time_1 == room_time) && (digitalRead(BEING) == LOW) ){x = 0;old_time_1 = millis();Serial.println("Varlık Algılanmadı, Röle Kapandı");}}
 // ***************************************** //
-if (x == 0) {digitalWrite(relay, LOW);}
+if (x == 0) {digitalWrite(relay, HIGH);}
 // ***************************************** //
-if (x == 1) {digitalWrite(relay, HIGH);}
+if (x == 1) {digitalWrite(relay, LOW);}
 // ***************************************** //
 if (digitalRead(BEING) == HIGH){old_time_1 = millis(); y = 1;}
 // ***************************************** //
